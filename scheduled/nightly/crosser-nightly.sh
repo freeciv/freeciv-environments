e9caae512096fd7866021639a16e226010b7e8f0
Marko Lindqvist <cazfi74@gmail.com>
2023-03-26 06:35:36 +0300

    crosser-nightly.sh: Add
    Initial version of nightly builds helper script to
    make crosser based builds using docker.
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ scheduled/nightly/crosser-nightly.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if ! test -x "$1/docker_run.sh" ; then
+  echo "$1 not a crosser docker scripts directory" >&2
+  exit 1
+fi
+
+if test "$2" != "main" &&
+   test "$2" != "S3_2" &&
+   test "$2" != "S3_1" &&
+   test "$2" != "S3_0" ; then
+  echo "Unknown branch \"$2\"" >&2
+  exit 1
+fi
+if test "$3" = "" ; then
+  echo "No Build number given" >&2
+  exit 1
+fi
+if test "$4" = "" ; then
+  echo "No commit given" >&2
+  exit 1
+fi
+
+MAINDIR="$(cd $(dirname $0) ; pwd)"
+DOCKDIR="$1"
+BRANCH="$2"
+BNBR="$3"
+SCOMMIT="$4"
+
+if ! test -f "${MAINDIR}/${BRANCH}/.git" ; then
+  echo "No git clone at \"${MAINDIR}/${BRANCH}\"" >&2
+  exit 1
+fi
+
+cd "$DOCKDIR"
+
+./docker_run.sh "${MAINDIR}/${BRANCH}"
+
+cd "$MAINDIR"
+
+if test "$BRANCH" != "S3_0" && test "$BRANCH" != "S3_1" ; then
+
+ls -1 ${BRANCH}/windows/installer_cross/meson/Output/Freeciv-*-setup.exe | ( while read ONAME ; do
+  NNAME=$(echo "$ONAME" | sed "s,-setup.exe,-$SCOMMIT-setup.exe,")
+  echo "$NNAME" | sed "s,^${BRANCH}/windows/installer_cross/meson/Output/,${BNBR}: crosser/," >> $BRANCH.files
+  mv "$ONAME" "$NNAME"
+  mv "$NNAME" "nightly/${BRANCH}/crosser/"
+  done )
+
+else
+  # Old S3_0 & S3_1 directory hierarchy
+ls -1 ${BRANCH}/windows/installer_cross/Output/Freeciv-*-setup.exe | ( while read ONAME ; do
+  NNAME=$(echo "$ONAME" | sed "s,-setup.exe,-$SCOMMIT-setup.exe,")
+  echo "$NNAME" | sed "s,^${BRANCH}/windows/installer_cross/Output/,${BNBR}: crosser/," >> $BRANCH.files
+  mv "$ONAME" "$NNAME"
+  mv "$NNAME" "nightly/${BRANCH}/crosser/"
+  done )
+fi
