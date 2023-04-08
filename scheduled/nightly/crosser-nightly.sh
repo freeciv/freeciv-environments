f6b016617573f4399c278e052fd11eb896346de1
Marko Lindqvist <cazfi74@gmail.com>
2023-04-08 22:19:40 +0300

    crosser-nightly.sh: Publish intermediate .7z packages
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scheduled/nightly/crosser-nightly.sh
+++ scheduled/nightly/crosser-nightly.sh
@@ -41,18 +41,32 @@ cd "$MAINDIR"
 if test "$BRANCH" != "S3_0" && test "$BRANCH" != "S3_1" ; then
 
 ls -1 ${BRANCH}/windows/installer_cross/meson/Output/Freeciv-*-setup.exe | ( while read ONAME ; do
-  NNAME=$(echo "$ONAME" | sed "s,-setup.exe,-$SCOMMIT-setup.exe,")
+  NNAME=$(echo "$ONAME" | sed "s,-setup.exe,-${SCOMMIT}-setup.exe,")
   echo "$NNAME" | sed "s,^${BRANCH}/windows/installer_cross/meson/Output/,${BNBR}: crosser/," >> $BRANCH.files
   mv "$ONAME" "$NNAME"
   mv "$NNAME" "nightly/${BRANCH}/crosser/"
   done )
 
+ls -1 ${BRANCH}/windows/installer_cross/meson/Output/freeciv-*.7z | ( while read ONAME ; do
+  NNAME=$(echo "$ONAME" | sed "s,.7z,-${SCOMMIT}.7z,")
+  echo "$NNAME" | sed "s,^${BRANCH}/windows/installer_cross/meson/Output/,${BNBR}: crosser/portable/," >> $BRANCH.files
+  mv "$ONAME" "$NNAME"
+  mv "$NNAME" "nightly/${BRANCH}/crosser/portable/"
+  done )
+
 else
   # Old S3_0 & S3_1 directory hierarchy
 ls -1 ${BRANCH}/windows/installer_cross/Output/Freeciv-*-setup.exe | ( while read ONAME ; do
-  NNAME=$(echo "$ONAME" | sed "s,-setup.exe,-$SCOMMIT-setup.exe,")
+  NNAME=$(echo "$ONAME" | sed "s,-setup.exe,-${SCOMMIT}-setup.exe,")
   echo "$NNAME" | sed "s,^${BRANCH}/windows/installer_cross/Output/,${BNBR}: crosser/," >> $BRANCH.files
   mv "$ONAME" "$NNAME"
   mv "$NNAME" "nightly/${BRANCH}/crosser/"
+  done )
+
+ls -1 ${BRANCH}/windows/installer_cross/Output/freeciv-*.7z | ( while read ONAME ; do
+  NNAME=$(echo "$ONAME" | sed "s,.7z,-${SCOMMIT}.7z,")
+  echo "$NNAME" | sed "s,^${BRANCH}/windows/installer_cross/Output/,${BNBR}: crosser/portable/," >> $BRANCH.files
+  mv "$ONAME" "$NNAME"
+  mv "$NNAME" "nightly/${BRANCH}/crosser/portable/"
   done )
 fi
