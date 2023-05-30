9c8c7a2a904bf25ccc8109008355f25006036f66
Marko Lindqvist <cazfi74@gmail.com>
2023-05-30 03:01:34 +0300

    Nightly: Add initial_setup.sh
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ scheduled/nightly/initial_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export STABLE_BRANCHES="S3_2 S3_1 S3_0"
+export REPO_URL="https://github.com/freeciv/freeciv"
+
+MAINDIR="$(cd $(dirname "$0") || exit 1 ; pwd)"
+
+for BRANCH in main $STABLE_BRANCHES
+do
+  if ! mkdir -p "nightly/${BRANCH}/crosser/portable" ||
+     ! mkdir -p "nightly/${BRANCH}/flatpak"          ||
+     ! mkdir -p "nightly/${BRANCH}/tarballs"         ||
+     ! mkdir -p "nightly/${BRANCH}/translations"
+  then
+    exit 1
+  fi
+done
+
+if ! git clone "$REPO_URL" main ; then
+  echo "Failed to clone freeciv git" >&2
+  exit 1
+fi
+
+(
+  cd main || exit 1
+  for BRANCH in $STABLE_BRANCHES
+  do
+    if ! git worktree add "../$BRANCH" "$BRANCH" ; then
+      exit 1
+    fi
+  done
+)
+
+echo
+echo "Now, setup e.g. cron to run nightly-*.sh and weekly-*.sh scripts."
+echo "You may also want to create ./upload.sh hook to upload"
+echo "produced contents of nightly/"
