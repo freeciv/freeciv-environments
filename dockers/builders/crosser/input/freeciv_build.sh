07b696546dade6fbf558dfd2d74d1a0be5e3bee1
Marko Lindqvist <cazfi74@gmail.com>
2023-06-20 23:16:37 +0300

    Crosser docker: Find installer_cross under platforms/windows
    Adapt to the move of the directory in freeciv repo.
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dockers/builders/crosser/input/freeciv_build.sh
+++ dockers/builders/crosser/input/freeciv_build.sh
@@ -2,7 +2,12 @@
 
 . /build/crosser-ver.sh
 
-cd /freeciv/windows/installer_cross/
+if test -d /freeciv/platforms/windows/installer_cross ; then
+  cd /freeciv/platforms/windows/installer_cross/
+else
+  # Older versions
+  cd /freeciv/windows/installer_cross/
+fi
 
 if test -x ./meson-build_all_installers.sh ; then
   ./meson-build_all_installers.sh "/usr/crosser/${CROSSER_DIR}" release
