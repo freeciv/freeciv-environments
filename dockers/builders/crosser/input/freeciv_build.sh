5716ddd63d43bcafaba3262d7829b8b11a0abd79
Marko Lindqvist <cazfi74@gmail.com>
2026-02-07 09:24:59 +0200

    freeciv_build.sh: Drop support for autotools builds
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dockers/builders/crosser/input/freeciv_build.sh
+++ dockers/builders/crosser/input/freeciv_build.sh
@@ -11,13 +11,9 @@ else
   if test -d /freeciv/platforms/windows/installer_cross ; then
     cd /freeciv/platforms/windows/installer_cross/
   else
-    # Older versions
+    # Freeciv < 3.3
     cd /freeciv/windows/installer_cross/
   fi
 
-  if test -x ./meson-build_all_installers.sh ; then
-    ./meson-build_all_installers.sh "/usr/crosser/${CROSSER_DIR}" release
-  else
-    ./build_all_installers.sh "/usr/crosser/${CROSSER_DIR}" release
-  fi
+  ./meson-build_all_installers.sh "/usr/crosser/${CROSSER_DIR}" release
 fi
