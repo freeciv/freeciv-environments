feb791134f78fbf1e0616a4016882ebe60fb65f4
Marko Lindqvist <cazfi74@gmail.com>
2026-02-07 09:29:42 +0200

    Update crosser-builder container to use crosser-2.11
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dockers/builders/crosser/input/crosser-ver.sh
+++ dockers/builders/crosser/input/crosser-ver.sh
@@ -1,5 +1,5 @@
 # Source this to scripts needing crosser version information
 
-CROSSER_VER="2.10"
+CROSSER_VER="2.11"
 CROSSER_DIR="win64stack-full-${CROSSER_VER}"
 CROSSER_PACKET="${CROSSER_DIR}.7z"
